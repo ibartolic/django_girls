a8cc248731127d2cedaeec5f314c97d644982e5c
ibartolic <ibartolic@gmail.com>
2018-02-26 10:32:59 +0100

    My Django Girls app, first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\etkigba\web_programming\djangogirls\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ blog_girls/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlogGirlsConfig(AppConfig):
+    name = 'blog_girls'
